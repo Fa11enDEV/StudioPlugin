24e0553c584b3c3336d28a652cb2bf8e0f713a7e
Fa11enDEV <hammesarthur82@gmail.com>
2024-12-18 10:02:15 -0300

    ver 0.1

---
+++ README.md
@@ -0,0 +1,41 @@
+# StudioPresence v3.0.4
+
+
+# Examples
+
+![image](https://github.com/iArxic/StudioPresence/assets/77511250/d23b22c2-af73-4f40-a2e8-7e5eeb4a0ada)
+
+# Installation
+
+• Download [Server Here](https://www.roblox.com) and [Roblox Plugin Here](https://www.roblox.com)  
+• Next, extract the already installed [Server](https://www.roblox.com) to a new folder (Inside desktop recommended).  
+• Run it!
+
+*Additional steps for **MacOS** users*
+1. Download the Mac server
+2. Extract it using the archive utility from the App Store
+3. [Open your terminal](https://support.apple.com/guide/terminal/open-or-quit-terminal-apd5265185d-f365-44cb-8b09-71a064a42125) and type `chmod u+x ` and then drag the server file into your terminal, press enter and it will turn the server into an executable file.
+4. Open the executable file! (If it cannot verify for malware go to settings > Privacy and Security, scroll down until you find the warning, and press open anyway)
+
+*Additional optional steps for **Windows** users*
+1. Locate the `startup.vbs` file
+2. Press Win + R and open `shell:startup`
+3. Drag `startup.vbs` into the folder that opens
+4. Edit `startup.vbs` with notepad
+5. Copy the path to the server exe file
+6. Replace `PATH_TO_EXE` with said path, make sure there is only one set of quotation marks  
+This will automatically start the server without opening a command window
+
+
+You might have an issue with the antivirus incorrectly flagging it as a virus, click "keep" and if needed exclude the file from your antivirus.
+
+_Note: The cmd window needs to be open in order for the plugin to work!_
+
+If the activity doesn't show up, double check you have activity privacy enabled!
+
+
+
+# Special thanks to
+
+
+
---
+++ dist/index.js
@@ -0,0 +1,83 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_rpc_1 = require("discord-rpc");
+const node_http_1 = __importDefault(require("node:http"));
+const chalk_1 = __importDefault(require("chalk"));
+const SERVER_PORT = 4455;
+const CLIENT_ID = "1028311936854675458";
+async function login(client) {
+    let user;
+    try {
+        user = (await client.login({
+            clientId: CLIENT_ID,
+        })).user;
+    }
+    finally {
+        if (!user) {
+            console.error(chalk_1.default.red("StudioPresence failed to start (Is Discord open?)"));
+        }
+        else {
+            console.log(chalk_1.default.green("StudioPresence started"));
+        }
+    }
+}
+(async () => {
+    const client = new discord_rpc_1.Client({ transport: "ipc" });
+    let lastTesting = 0;
+    login(client);
+    node_http_1.default
+        .createServer((req, res) => {
+        let data = "";
+        req.on("data", (additionalData) => {
+            data += additionalData;
+        });
+        req.on("end", () => {
+            try {
+                let passThrough = true;
+                try {
+                    data = JSON.parse(data).activity;
+                }
+                catch (ignored) {
+                    data = undefined;
+                }
+                if (!data) {
+                    client.clearActivity();
+                }
+                else {
+                    if (data.details === "Testing") {
+                        lastTesting = Date.now();
+                    }
+                    else if (Date.now() - lastTesting < 3000) {
+                        // i wish i could just use a return here
+                        passThrough = false;
+                    }
+                    if (passThrough) {
+                        client.setActivity({
+                            details: data.details,
+                            startTimestamp: data.timestamps.start,
+                            state: data.state,
+                            largeImageText: data.assets.large_text,
+                            largeImageKey: data.assets.large_image,
+                            smallImageText: data.assets.small_text,
+                            smallImageKey: data.assets.small_image,
+                        });
+                        if (data.updateType === "CLOSE")
+                            client.clearActivity().catch(() => null);
+                    }
+                }
+                res.writeHead(200, { "Content-Type": "text/plain" });
+                res.end("SET Activity");
+            }
+            catch (err) {
+                console.error(err);
+                client
+                    .clearActivity()
+                    .catch(() => console.error(chalk_1.default.red("Failed to clear activity")));
+            }
+        });
+    })
+        .listen(SERVER_PORT);
+})();
